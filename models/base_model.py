c7ffeb1f700a9186cc60dd100c5f4bce7872f35f
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-08-07 11:11:22 +0000

    base model task 3

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# class BaseModel that defines all common attributes/methods for other classes
+
+import uuid
+import datetime
+
+
+class BaseModel:
+    """ defines all common attributes/methods for other classes """
+    def __init__(self):
+        """ __init__(self): instantiate instance """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """ __str__(self): string representation of the class """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+            save(self): updates the public instance attribute updated_at
+            with the current datetime
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+           to_dict(self): returns a dictionary containing all keys/values of
+           __dict__ of the instance
+        """
+        dict1 = {}
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                value = value.isoformat()
+            dict1[key] = value
+        dict1['__class__'] = self.__class__.__name__
+        return dict1
